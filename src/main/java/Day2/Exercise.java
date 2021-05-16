882143f39d187c70c62f8f38870aaf3536869185
crdiam <angiziewska@gmail.com>
2021-05-16 18:55:37 +0200

    function isPrime() added

--- src/main/java/Day2/Exercise.java
+++ src/main/java/Day2/Exercise.java
@@ -11,12 +11,23 @@ public class Exercise {
     }
 
     public static boolean isPrime(int number) {
-        //Hausaufgabe
-        return true;
+        if (number==0 || number==1) {
+            return false;
+        }
+        else if(number==2 || number==3 || number==5){
+            return true;
+        }
+        else if (number%2==0 || number%3==0 || number%5==0) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     public static void main(String[] args) {
         Exercise e = new Exercise(); //It's possible to do static instead
-        System.out.println(e.sumEven(6));
+        System.out.println(isPrime(31));
+        //System.out.println(e.sumEven(6));
     }
 }
