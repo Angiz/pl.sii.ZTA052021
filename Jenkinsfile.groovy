c9e1baab37709b8d8d3d4223c49af4cbeb58dd56
AnetaGiziewska <aneta.giziewska@netreveal.ai>
2023-09-12 11:28:20 +0200

    [ci skip] Post out of Stages section in Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -33,26 +33,25 @@ pipeline {
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
         }
-
-        post {
-            // If Maven was able to run the tests, even if some of the test
-            // failed, record the test results and archive the jar file.
-            success {
-                junit '**/target/surefire-reports/TEST-*.xml'
-                archiveArtifacts 'target/*.jar'
-                slackSend(color: 'good', message:
-                        """Build success!
+    }
+    post {
+        // If Maven was able to run the tests, even if some of the test
+        // failed, record the test results and archive the jar file.
+        success {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts 'target/*.jar'
+            slackSend(color: 'good', message:
+                    """Build success!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-            }
-            failure {
-                junit '**/target/surefire-reports/TEST-*.xml'
-                archiveArtifacts 'target/*.jar'
-                slackSend(color: 'bad', message:
-                        """Build failed!
+        }
+        failure {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts 'target/*.jar'
+            slackSend(color: 'bad', message:
+                    """Build failed!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-            }
         }
     }
 }
