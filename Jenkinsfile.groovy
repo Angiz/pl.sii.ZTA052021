33cabf3596c606b3e680bdf732ed0260e647454f
AnetaGiziewska <aneta.giziewska@netreveal.ai>
2023-09-12 11:18:05 +0200

    [ci skip] Updating Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -30,26 +30,26 @@ pipeline {
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
+        }
 
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                    slackSend (color: 'good', message: 
-                    """Build success!
+        post {
+            // If Maven was able to run the tests, even if some of the test
+            // failed, record the test results and archive the jar file.
+            success {
+                junit '**/target/surefire-reports/TEST-*.xml'
+                archiveArtifacts 'target/*.jar'
+                slackSend(color: 'good', message:
+                        """Build success!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-                }
-                failure {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                    slackSend (color: 'bad', message:
-                    """Build failed!
+            }
+            failure {
+                junit '**/target/surefire-reports/TEST-*.xml'
+                archiveArtifacts 'target/*.jar'
+                slackSend(color: 'bad', message:
+                        """Build failed!
                     BUILD_TAG: ${BUILD_TAG} 
                     BUILD_URL: ${BUILD_URL}""")
-                }
             }
         }
     }
